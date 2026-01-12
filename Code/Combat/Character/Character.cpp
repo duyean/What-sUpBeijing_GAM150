08c8732db1d75058386a34af6b6382791ad0bed3
The-Sane-Lion <69781872+Nocturnalatic@users.noreply.github.com>
2026-01-12 16:26:44 +0800

    Reapply "Merge"
    This reverts commit c0161c4a46a268231fb2b21aecc7f37bb06cbe6f.

--- Code/Combat/Character/Character.cpp
+++ Code/Combat/Character/Character.cpp
@@ -9,5 +9,15 @@ void Character::DealDamage(Character* target, float coefficient)
 
 void Character::TakeDamage(float incomingDamage)
 {
+	float defDMGReduction = def / (def + Game::DEF_CONSTANT);
+	float finalDamageTaken = incomingDamage * (1 - defDMGReduction) * (1 - dmgReduction);
+	hp -= finalDamageTaken;
+	//Check death here or something
+}
 
+void Character::UpdateAttributes(void)
+{
+	maxHP = baseMaxHP * (1 + maxHPBonus);
+	atk = baseATK * (1 + atkBonus);
+	def = baseDEF * (1 + defBonus);
 }

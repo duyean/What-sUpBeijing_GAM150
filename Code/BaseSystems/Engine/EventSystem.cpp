ae404d9d2e19196087e765ec5702e04c0fdfcb20
tqy-03 <taiqianyi1@gmail.com>
2026-01-25 23:54:55 +0800

    Added UI event system (incomplete)

---
+++ Code/BaseSystems/Engine/EventSystem.cpp
@@ -0,0 +1,28 @@
+#include "EventSystem.hpp"
+#include <cmath>
+#include "../EaseFunctions/AEVEC2OVERLOAD.hpp"
+
+
+void EventSystem::addUIElement(UIElement* ui)
+{
+	uiElements.push_back(ui);
+}
+
+void EventSystem::removeUIElement(UIElement* ui)
+{
+	uiElements.erase(
+		std::remove(uiElements.begin(), uiElements.end(), ui),
+		uiElements.end()
+	);
+}
+
+bool EventSystem::pointOverlap(UIElement* ui)
+{
+	AEInputGetCursorPosition(m_x, m_y);
+
+}
+
+void EventSystem::Update(double dt)
+{
+
+}
---
+++ Code/BaseSystems/Engine/UIElement.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef _UIElement_
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <crtdbg.h> // To check for memory leaks
+#include <mutex>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include "AEEngine.h"
+#include "OOP.hpp"
+#include "Entity.hpp"
+#include "Colors.hpp"
+
+class UIElement : public SoloBehavior
+{
+public:
+	AEVec2 size;
+	AEVec2 pos;
+
+	UIElement(){}
+	~UIElement(){};
+};
+
+
+#endif

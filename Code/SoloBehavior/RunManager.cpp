79cf451c7c15922b869f39b3e4a1af20f7116666
The-Sane-Lion <69781872+Nocturnalatic@users.noreply.github.com>
2026-02-27 13:40:39 +0800

    Added comments

--- Code/SoloBehavior/RunManager.cpp
+++ Code/SoloBehavior/RunManager.cpp
@@ -1,3 +1,13 @@
+/*!
+@file RunManager.hpp
+@author Wayne Lion (lion.w)
+@course CSD11451
+@section B
+@Final Project
+@date 25/2/26
+@brief
+This file contains the implementation for a Run Manager for our game.
+*//*______________________________________________________________________*/
 #include "RunManager.hpp"
 
 void RunManager::StartRun()
@@ -7,11 +17,13 @@ void RunManager::StartRun()
 
 void RunManager::ResetRun()
 {
+	//Clear the blessings for the current run
 	runBlessings.clear();
 }
 
 void RunManager::AddBlessing(std::unique_ptr<Blessing> bless)
 {
+	//Add a new blessing, the original blessing from the database is cloned
 	runBlessings.push_back(std::move(bless));
 }
 
